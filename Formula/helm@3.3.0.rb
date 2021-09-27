72fad6e29cb633422792dbcf42cd562305cd47f1
Vijendra kumar <vijendra.kumar@nintex.com>
2021-09-27 12:43:22 +1000

    helm 3.30

---
+++ Formula/helm@3.3.0.rb
@@ -0,0 +1,38 @@
+class HelmAT330 < Formula
+  desc "The Kubernetes package manager"
+  homepage "https://helm.sh/"
+  url "https://github.com/helm/helm.git",
+      tag:      "v3.3.0",
+      revision: "8a4aeec08d67a7b84472007529e8097ec3742105"
+  license "Apache-2.0"
+  head "https://github.com/helm/helm.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install "bin/helm"
+
+    mkdir "man1" do
+      system bin/"helm", "docs", "--type", "man"
+      man1.install Dir["*"]
+    end
+
+    output = Utils.safe_popen_read({ "SHELL" => "bash" }, bin/"helm", "completion", "bash")
+    (bash_completion/"helm").write output
+
+    output = Utils.safe_popen_read({ "SHELL" => "zsh" }, bin/"helm", "completion", "zsh")
+    (zsh_completion/"_helm").write output
+  end
+
+  test do
+    system "#{bin}/helm", "create", "foo"
+    assert File.directory? "#{testpath}/foo/charts"
+
+    version_output = shell_output("#{bin}/helm version 2>&1")
+    assert_match "Version:\"v#{version}\"", version_output
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output
+    end
+  end
+end
